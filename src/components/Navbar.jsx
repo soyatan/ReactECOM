61bf6f845376845d3d5734fe54dffadd0446dac7
Necati Soyata <necati.soyata@gmail.com>
2022-02-02 13:54:02 +0300

    "create navbar"

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import styled from "styled-components";
+import { SearchIcon } from "@mui/icons-material/Search";
+import { ShoppingCartOutlined } from "@mui/icons-material";
+import { Search } from "@material-ui/icons";
+import { Badge } from "@material-ui/core";
+
+const Container = styled.div`
+  height: 60px;
+`;
+
+const Input = styled.input`
+  border: none;
+`;
+const Logo = styled.h1`
+  font-weight: bold;
+  text-align: center;
+`;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`;
+
+const SearchContainer = styled.div`
+  border: 1px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 20px;
+  padding: 5px;
+`;
+const Center = styled.div`
+  flex: 2;
+`;
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+`;
+
+const Navbar = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>E</Language>
+          <SearchContainer>
+            <Input />
+            <Search style={{ color: "gray", fontSize: 16 }} />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>NECOTO</Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlined />
+            </Badge>
+          </MenuItem>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Navbar;
