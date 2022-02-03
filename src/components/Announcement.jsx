a46d245cf1e9bbba1a260653f93db238b9845f5e
Necati Soyata <necati.soyata@gmail.com>
2022-02-03 17:37:23 +0300

    "add slides"

--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -4,6 +4,11 @@ const Container = styled.div`
   height: 30px;
   background-color: teal;
   color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 14px;
+  font-weight: bold;
 `;
 
 const Announcement = () => {
